730af64510303c577b6bad8cc4ff0f865ec465d0
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-08-14 23:19:58 -0500

    integracion del bulma para los estilos css

--- index.php
+++ index.php
@@ -6,8 +6,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sistema de Inventario</title>
+    <link rel="stylesheet" href="./css/bulma.min.css">
 </head>
 <body>
 <h1>Sistema de Inventario</h1>
+<div class="container">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores iure quod sunt? Fuga magnam nobis non
+        reiciendis repellendus soluta tenetur? Corporis esse facilis iusto mollitia, nesciunt quam vel. Eius, suscipit.
+    </p>
+    <button class="button is-primary">Mi boton Bulma</button>
+</div>
 </body>
 </html>
