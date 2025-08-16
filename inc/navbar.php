4bd1dcdc19348768f4b8e3e521c11fa1e548b11c
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-08-15 23:41:54 -0500

    dropdown para usuarios, categorias y productos

--- inc/navbar.php
+++ inc/navbar.php
@@ -25,13 +25,48 @@
                 <a class="navbar-link">
                     Usuarios
                 </a>
-
                 <div class="navbar-dropdown">
                     <a class="navbar-item">
                         Nuevo
                     </a>
                     <a class="navbar-item">
                         Lista
+                    </a>
+
+                    <a class="navbar-item">
+                        Buscar
+                    </a>
+                </div>
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    Categorias
+                </a>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                        Nuevo
+                    </a>
+                    <a class="navbar-item">
+                        Lista
+                    </a>
+                    <a class="navbar-item">
+                        Buscar
+                    </a>
+                </div>
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    Productos
+                </a>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                        Nuevo
+                    </a>
+                    <a class="navbar-item">
+                        Lista
+                    </a>
+                    <a class="navbar-item">
+                        Por Categoria
                     </a>
                     <a class="navbar-item">
                         Buscar
